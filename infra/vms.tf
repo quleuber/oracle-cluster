ac6420479286ae2a822bef6ba6efe2e6e317c07f
Kelvin Santos <kelvinsteinersantos@gmail.com>
2021-08-21 19:24:03 -0300

    worker nodes

--- infra/vms.tf
+++ infra/vms.tf
@@ -71,6 +71,7 @@ resource "oci_core_instance" "cluster_master_node" {
   metadata = {
     "ssh_authorized_keys" = var.ssh_authorized_keys
   }
+  extended_metadata = {}
 
   agent_config {
     # are_all_plugins_disabled = "false"
@@ -91,11 +92,85 @@ resource "oci_core_instance" "cluster_master_node" {
   }
 }
 
-output "instances_ip_addr" {
-  value = { for name, inst in oci_core_instance.cluster_master_node :
-    (inst.display_name) => {
-      "private" = inst.private_ip
-      "public"  = inst.public_ip
+resource "oci_core_instance" "cluster_worker_node" {
+  count = var.num_worker_nodes
+
+  display_name   = "cluster-worker-node-${format("%02d", count.index + 1)}"
+  compartment_id = var.compartment_ocid
+
+  state = "RUNNING"
+
+  source_details {
+    source_type = "image"
+    source_id   = var.images["oracle-linux-8"]["aarch64"][var.region]
+  }
+
+  shape = "VM.Standard.A1.Flex"
+  shape_config {
+    baseline_ocpu_utilization = ""
+    memory_in_gbs             = "6"
+    ocpus                     = "1"
+  }
+
+  availability_domain = var.availability_domain
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
+
+  create_vnic_details {
+    display_name   = "cluster-worker-node-${format("%02d", count.index + 1)}"
+    hostname_label = "cluster-worker-node-${format("%02d", count.index + 1)}"
+    subnet_id      = oci_core_subnet.public_subnet.id
+    # assign_public_ip = "true"
+  }
+
+  launch_options {
+    boot_volume_type                    = "PARAVIRTUALIZED"
+    firmware                            = "UEFI_64"
+    is_consistent_volume_naming_enabled = "true"
+    is_pv_encryption_in_transit_enabled = "true"
+    network_type                        = "PARAVIRTUALIZED"
+    remote_data_volume_type             = "PARAVIRTUALIZED"
+  }
+
+  metadata = {
+    "ssh_authorized_keys" = var.ssh_authorized_keys
+  }
+  extended_metadata = {}
+
+  agent_config {
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Run Command"
     }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    # plugins_config {
+    #   desired_state = "DISABLED"
+    #   name          = "Bastion"
+    # }
+  }
+}
+
+output "instances_ip_addr" {
+  value = {
+    master = {
+      for name, inst in oci_core_instance.cluster_master_node :
+      (inst.display_name) => {
+        "group"   = "master"
+        "private" = inst.private_ip
+        "public"  = inst.public_ip
+      }
+    },
+    worker = {
+      for name, inst in oci_core_instance.cluster_worker_node :
+      (inst.display_name) => {
+        "group"   = "worker"
+        "private" = inst.private_ip
+        # "public"  = inst.public_ip
+      }
+    },
   }
 }
